e2328ac44c436ae77efd9dd755f8dfea3fadfd76
Timon van Spronsen <timonvanspronsen@outlook.com>
2017-01-29 12:09:00 +0100

    Only update cookies if current cookie expires

--- CrunchyrollUnblocker.safariextension/main.js
+++ CrunchyrollUnblocker.safariextension/main.js
@@ -1,5 +1,11 @@
-function setCookie(key, value) {
-	document.cookie = key + '=' + value
+function setCookie (key, value) {
+  document.cookie = key + '=' + value
+}
+
+function getCookie (key) {
+  var value = '; ' + document.cookie
+  var parts = value.split('; ' + key + '=')
+  if (parts.length >= 2) return parts.pop().split(';').shift()
 }
 
 function listen (event) {
@@ -8,8 +14,30 @@ function listen (event) {
   setCookie('sess_id', event.message)
 }
 
+function isUS () {
+  var us = new RegExp('United States of America')
+  var location = document.getElementById('footer_country_flag')
+  if (us.test(location.alt)) {
+    return true
+  }
+}
+
+function main () {
+  var time = localStorage.getItem('LAST_UPDATED')
+  var SECONDS_IN_HOUR = 3600
+
+  // Don't update cookies if locale is still set to United States
+  if ((time + (SECONDS_IN_HOUR * 3)) > (Date.now() || isUS())
+    && getCookie('sess_id')) return
+
+  NOW = Date.now()
+  localStorage.setItem('LAST_UPDATED', NOW)
+
+  // Request cookie from global page
+  safari.self.tab.dispatchMessage('getCookie')
+}
+
+main()
+
 // Listen for messages from global page
 safari.self.addEventListener('message', listen, false)
-
-// Request cookie from global page
-safari.self.tab.dispatchMessage('getCookie')
